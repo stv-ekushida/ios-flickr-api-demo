0c2dfab4820984bc0173110663e98e6ebe98ce62
sdt-kushida <e.kushida@st-ventures.jp>
2016-12-17 20:20:28 +0900

    remove pod file

--- Pods/Target Support Files/Pods-ios-flickr-api-demoTests/Pods-ios-flickr-api-demoTests-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_ios_flickr_api_demoTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_ios_flickr_api_demoTestsVersionString[];
-
